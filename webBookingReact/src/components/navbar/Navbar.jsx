9f39d61822d78fe8ee5b770863996ec3db202b91
Pankaj97825 <Pankaj.k6367@gmail.com>
2025-08-24 01:13:33 +0530

    Fix import path for Navabar component (case-sensitive)

--- webBookingReact/src/components/navbar/Navbar.jsx
+++ webBookingReact/src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import './navbar.css'
+import './Navbar.css'
 import {Link} from "react-router-dom"
 import { AuthContext } from '../../context/AuthContext';
 const Navbar = () => {
